7f396c08e23fc3fc43ba8f16943cf59a7dc71882
gitngp <nunogouveiap@gmail.com>
2020-06-11 12:40:03 +0100

    added a text field

--- app/src/main/java/com/nunogp/androidtestgit/MainActivity.kt
+++ app/src/main/java/com/nunogp/androidtestgit/MainActivity.kt
@@ -8,6 +8,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
        var ocupation = "developer"
-        var name = "jonnyB"
+       var name = "jonnyB"
     }
 }
